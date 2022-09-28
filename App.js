a137cc4fca2380badb5fcd4432ed9286612e46c2
Inkyung <chelsea1201@naver.com>
2022-09-28 11:44:58 +0900

    버튼 추가

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, View, ScrollView, StyleSheet } from "react-native"; //view, scrollview, stylesheet 쓸거임
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+const App = () => {
+  return (<>
+    <View style={[styles.container, styles.playingSpace]}>
+
     </View>
-  );
+    <ScrollView style={[styles.container]}>
+      <View style={[styles.controlSpace]}>
+        <View style={[styles.buttonView]}>
+          <Button title="1"/>
+        </View >
+        <View style={[styles.buttonView]}>
+          <Button title="2"/>
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="3"/>
+        </View>
+        <View style={[styles.buttonView]}> 
+          <Button title="4"/>
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="5"/>
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="6"/>
+        </View>
+        <View style={[styles.buttonView]}>
+          <Button title="7"/>
+        </View>
+      </View>
+    </ScrollView>
+
+  </>);
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({  //여기에 스타일 만들고 위에 view에 연결해줘야함
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    height:'50%' //높이 반반으로
   },
+  playingSpace:{
+    backgroundColor: 'lightyellow',
+    borderColor: 'blue',
+    borderWidth: 3
+  },
+  controlSpace: {
+    backgroundColor: '#A5A5A5',
+    flexDirection: 'row', 
+    flexWrap: 'wrap'
+  },
+  buttonView: {
+    width: '50%',
+    padding: 10
+  }
 });
+
+export default App;
