3738451a89389e164e6c473e8145004e56e59db3
Inkyung <chelsea1201@naver.com>
2022-10-12 17:59:29 +0900

    console.log

--- App.js
+++ App.js
@@ -27,25 +27,43 @@ const App = () => {
     <ScrollView style={[styles.container]}>
       <View style={[styles.controlSpace]}>
         <View style={[styles.buttonView]}>
-          <Button title="CHANGE FLEX DIRECTION"/>
+          <Button 
+            title="CHANGE FLEX DIRECTION"
+            onPress={() => console.log("CHANGE FLEX DIRECTION")}
+          />
         </View >
         <View style={[styles.buttonView]}>
-          <Button title="CHANGE JUSTIFY CONTENT"/>
+          <Button 
+            title="CHANGE JUSTIFY CONTENT"
+            onPress={() => console.log("CHANGE JUSTIFY CONTENT")}
+          />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title="CHANGE ALIGN ITEMS"/>
+          <Button 
+            title="CHANGE ALIGN ITEMS"
+            onPress={() => console.log("CHANGE ALIGN ITEMS")}
+          />
         </View>
         <View style={[styles.buttonView]}> 
-          <Button title="CHANGE DIRECTION"/>
+          <Button 
+          title="CHANGE DIRECTION"
+          onPress={() => console.log("CHANGE DIRECTION")}
+          />
         </View>
         <View style={[styles.buttonView]}>
-          <Button title="CHANGE FLEX WRAP"/>
+          <Button 
+          title="CHANGE FLEX WRAP"
+          onPress={() => console.log("CHANGE FLEX WRAP")}/>
         </View>
         <View style={[styles.buttonView]}>
-          <Button title="ADD SQURE"/>
+          <Button 
+          title="ADD SQURE"
+          onPress={() => console.log("ADD SQURE")}/>
         </View>
         <View style={[styles.buttonView]}>
-          <Button title="DELETE SQUARE"/>
+          <Button 
+          title="DELETE SQUARE"
+          onPress={() => console.log("DELETE SQUARE")}/>
         </View>
       </View>
     </ScrollView>
