c97e2f5bb24bdf88a05e1d8c9bc3319dd378a1c8
Inkyung <chelsea1201@naver.com>
2022-11-16 11:27:59 +0900

    개구리, TouchableOpacity

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { Text, Button } from 'react-native';
+import { Text, Button, Image, View, StyleSheet, TouchableOpacity } from 'react-native';
 
 function Home ( {navigation} ) {
-  return (
-    <>
+  return (  // 기존<> 에서 <View>로 바꾸면 스타일 적용 가능!
+    <>    
       <Text> Hello Home </Text>
+      <Image
+        source={require('../assets/frog-1371919.png')}    // 개구리 이미지 삽입
+        style={{width:400, height:400}}
+      />
+
+      <TouchableOpacity
+        style={styles.buttonContainer}                    // 버튼 박스 생성, 스타일은 buttonContainer에서 설정
+        onPress={() => navigation.navigate('Layout')}     // 누르면 Layout으로 이동
+      >
+        <Text style={styles.buttonTest}>Go To Layout</Text>     
+      </TouchableOpacity>
       <Button
         title = "Go to Layout"
         onPress={() => navigation.navigate('Layout')}
@@ -12,5 +23,18 @@ function Home ( {navigation} ) {
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonContainer:{
+    backgroundColor:'black',
+    borderRadius:5,
+    padding:10,
+    margin:20
+  },
+  buttonTest:{
+    fontSize:20,
+    color:'#fff'
+  }
+})
 
 export default Home;
